f2005aa865463615051ac117794029683b043025
perkles <jociele@hotmail.com>
2022-07-16 18:11:52 -0300

    Boas praticas de variaveis

---
+++ src/br/variaveis/Variaveis.java
@@ -0,0 +1,19 @@
+package br.variaveis;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int quantidades = 67;
+        final int NUMEROS_TENTATIVAS = 2;
+
+        System.out.println(i);
+        System.out.println(quantidades);
+        System.out.println(NUMEROS_TENTATIVAS);
+
+
+
+    }
+}
